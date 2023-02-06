5f54906c0d83b66964d1e282a7d518e1b7217ad2
John Carlo Fababeir <jcfababeir09@gmail.com>
2023-02-06 12:26:49 +0800

    [ ADD ] - Add team member local img

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,3 +1,9 @@
+import Dawal from '../Assets/img/team-member/dawal.jpg'
+import Fababeir from '../Assets/img/team-member/fababeir.jpg'
+import Florita from '../Assets/img/team-member/florita.jpg'
+import Lotao from '../Assets/img/team-member/lotao.jpg'
+import Mariano from '../Assets/img/team-member/mariano.jpg'
+
 const API = {
     nav_menu: [
         {
@@ -16,8 +22,7 @@ const API = {
     team_member: [
         {
             name: 'John Carlo T. Fababeir',
-            img_src:
-                'https://scontent.fmnl17-3.fna.fbcdn.net/v/t39.30808-6/326952720_1318198072071760_5370413389779193775_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGw4DptIt3Tsqn_G4uVkbVQ4Rso_x5PMALhGyj_Hk8wAmBhnADIC5M5uiR8Aqzdz5ktHLGhzjKIUfNbxK7dix_W&_nc_ohc=cyu3ZiGZmAcAX-EfjE5&_nc_ht=scontent.fmnl17-3.fna&oh=00_AfDRyFMc3UyI60EL7czrlsRfIbUNOkunPDi2E9kgejae3w&oe=63DADD51',
+            img_src: Fababeir,
             email: 'johncarlofababeir@outlook.com',
             badge: [
                 {
@@ -32,9 +37,8 @@ const API = {
         },
         {
             name: 'Cedric Paul B. Dawal',
-            img_src:
-                'https://scontent.fmnl17-1.fna.fbcdn.net/v/t39.30808-6/313267705_2370945246387670_7216795786553429154_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEIa0BAdJkJFDqtVyqbM850VMJWuCesaC5Uwla4J6xoLhXFfSyla2JlVun4MQaUHBUjd84TYf1iS_jwTXC7ePdA&_nc_ohc=IOG-OySjVOoAX9V0xWX&_nc_ht=scontent.fmnl17-1.fna&oh=00_AfAw5OBHqSmN0_dtANKhkee8nf-DFjSA1NUnk3NtpFZqtw&oe=63DB9128',
-            email: 'bernabecedric1@gmail.com',
+            img_src: Dawal,
+			email: 'bernabecedric1@gmail.com',
             badge: [
                 {
                     color: 'warning',
@@ -44,9 +48,8 @@ const API = {
         },
         {
             name: 'Romano D. Lotao',
-            img_src:
-                'https://scontent.fmnl17-4.fna.fbcdn.net/v/t39.30808-6/308127917_1547337495695310_8584138807451987305_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH0rIWZ-_InA-PRfrWgnuU_0l6BRbm2YcnSXoFFubZhyaMPKojL-cKptPW0UbcnVuD81Cr3mjzaU5V7YGNI5Bd1&_nc_ohc=SMoCslyH34wAX9UwlzI&_nc_ht=scontent.fmnl17-4.fna&oh=00_AfAqUpihQF3uSFgTlephNDeaKMA6JXF2X4fN77sfstaEZQ&oe=63DC0C88',
-            email: 'lutaolester10@gmail.com',
+            img_src: Lotao,
+			email: 'lutaolester10@gmail.com',
             badge: [
                 {
                     color: 'warning',
@@ -56,9 +59,8 @@ const API = {
         },
         {
             name: 'Crischell Jay D. Florita',
-            img_src:
-                'https://scontent.fmnl17-2.fna.fbcdn.net/v/t39.30808-1/324724465_918196819551340_9131902046718500937_n.jpg?stp=c0.0.200.200a_dst-jpg_p200x200&_nc_cat=107&ccb=1-7&_nc_sid=f67be1&_nc_eui2=AeGbDK__MAhH1UghXjaujx5ZQVu1jnIKkBtBW7WOcgqQGy7W41yds8J2cuYhLxKHd0s2zfit47EJp1L7YhH_0wwN&_nc_ohc=5lxr99U8JJcAX_T2DJf&_nc_ht=scontent.fmnl17-2.fna&oh=00_AfC9NxYk446f25BYGkLF3NApe3vPRvkhUoeqN0aCrkSrTQ&oe=63DAF119',
-            email: 'cjflorita01@gmail.com',
+            img_src: Florita,
+			email: 'cjflorita01@gmail.com',
             badge: [
                 {
                     color: 'warning',
@@ -68,9 +70,8 @@ const API = {
         },
         {
             name: 'Fely L. Mariano',
-            img_src:
-                'https://scontent.fmnl17-1.fna.fbcdn.net/v/t39.30808-1/272173417_1078366119621862_7910754391263121412_n.jpg?stp=dst-jpg_s200x200&_nc_cat=101&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeH9tnbKGdduoZgQMSAuWJx_P4ixbiWZpjw_iLFuJZmmPGHId9pK0QZ1SCHheu8MUqfH9FdQr2WZHSsGmuWRP7_g&_nc_ohc=Nw2G90aMOOMAX-GzmwA&_nc_ht=scontent.fmnl17-1.fna&oh=00_AfB3CuG_e49GqLYKZ2YgR56CZ_hmZKzqy5E1Ik5gl_IXhg&oe=63DA960F',
-            email: 'felymariano0211@gmail.com',
+            img_src: Mariano,
+			email: 'felymariano0211@gmail.com',
             badge: [
                 {
                     color: 'warning',
