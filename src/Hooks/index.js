59175ab4296563c6df525b4a42cd422db6ff9ea0
jcavenue <jcfababeir09@gmail.com>
2023-02-24 11:11:15 +0800

    [ REF ] - Refactor

--- src/Hooks/index.js
+++ src/Hooks/index.js
@@ -1,6 +1,5 @@
 import useCreateUser from './useCreateUser'
 import useLoginUser from './useLoginUser'
 import useStateStorage from './useStateStorage'
-import useFetch from './useFetch'
 
-export { useCreateUser, useLoginUser, useStateStorage, useFetch }
+export { useCreateUser, useLoginUser, useStateStorage }
